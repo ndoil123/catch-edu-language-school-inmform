8c35e88fb3aa807f042c28979d0d58a425c8a718
Sangwoo rhie <powercom92@naver.com>
2025-04-24 21:11:55 +0900

    commit

--- school/src/app.module.ts
+++ school/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SheetModule } from './sheet/sheet.module';
 
 @Module({
-  imports: [],
+  /* .env 로딩을 전역으로 활성화 */
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SheetModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
